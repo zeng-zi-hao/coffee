9cc196afd1cb2695982030e9f40a08bd4d29895b
zeng-zi-hao <70184451+zeng-zi-hao@users.noreply.github.com>
2024-04-26 13:03:14 +0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -221,8 +221,8 @@ const animationTimeline = () => {
       "-=2"
     )
     .from(".hat", 0.5, {
-      x: -100,
-      y: 350,
+      x: -60,
+      y: 330,
       rotation: -180,
       opacity: 0
     })
